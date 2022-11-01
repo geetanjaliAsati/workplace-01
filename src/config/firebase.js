bc63886714a296b6d0c7d3fe859361715bda3ba3
Geetanjali <asatigeetanjali@gmail.com>
2022-11-01 13:46:13 +0530

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCr4bZlDbHh6wnZ8gO0ZgXNPg5AJDwK5nw",
+  authDomain: "workplace-website-01.firebaseapp.com",
+  projectId: "workplace-website-01",
+  storageBucket: "workplace-website-01.appspot.com",
+  messagingSenderId: "1061070737647",
+  appId: "1:1061070737647:web:901569bb1e31680fac8b1a",
+  measurementId: "G-46R1VNJ7KX"
+};
+export default firebaseConfig;
+
+
+
+
+
+/**
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCr4bZlDbHh6wnZ8gO0ZgXNPg5AJDwK5nw",
+  authDomain: "workplace-website-01.firebaseapp.com",
+  projectId: "workplace-website-01",
+  storageBucket: "workplace-website-01.appspot.com",
+  messagingSenderId: "1061070737647",
+  appId: "1:1061070737647:web:901569bb1e31680fac8b1a",
+  measurementId: "G-46R1VNJ7KX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+ */
